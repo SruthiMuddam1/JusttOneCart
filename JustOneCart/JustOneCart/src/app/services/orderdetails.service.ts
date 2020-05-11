eee5012d0ba046b15c31dde7fa4b69f51b8512d2
SruthiMuddam1 <64985206+SruthiMuddam1@users.noreply.github.com>
2020-05-11 14:44:25 -0400

    added in services folder

---
+++ JustOneCart/JustOneCart/src/app/services/orderdetails.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';  
+import { HttpClient } from '@angular/common/http';  
+import { HttpHeaders } from '@angular/common/http';  
+import { Observable } from 'rxjs';  
+import { Product } from '../models/product'; 
+import { CSVRecord } from '../models/csvrecord';  
+
+@Injectable({  
+  providedIn: 'root'  
+})  
+  
+export class OrderdetailsService {  
+  url = 'https://localhost:44334/';    
+  constructor(private http: HttpClient) { }  
+
+  createOrder(csvrecord: CSVRecord): Observable<CSVRecord> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };  
+    return this.http.post<CSVRecord>(this.url+'api/ProductOrders',  
+    csvrecord, httpOptions);  
+  }  
+  getOrderDetails() : Observable<CSVRecord[]>
+  {    
+    debugger
+    console.log(this.http.get<CSVRecord[]>(this.url+'api/ProductOrders'));
+    return this.http.get<CSVRecord[]>(this.url+'api/ProductOrders');
+  }
+  getOrderDetailsbyId(id : Number): Observable<CSVRecord>
+  {
+    return this.http.get<CSVRecord>(this.url + 'api/ProductOrders/'+id);
+  }
+  
+} 
---
+++ JustOneCart/JustOneCart/src/app/services/product.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';  
+import { HttpClient } from '@angular/common/http';  
+import { HttpHeaders } from '@angular/common/http';  
+import { Observable } from 'rxjs';  
+import { Product } from '../models/product';  
+import { CSVRecord } from '../models/csvrecord';  
+
+@Injectable({  
+  providedIn: 'root'  
+})  
+  
+export class ProductService {  
+  url = 'https://localhost:44334/';    
+  constructor(private http: HttpClient) { }  
+  getAllProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url + 'Api/Product/AllProducts');  
+  }  
+  getProductById(ID: Number): Observable<Product> {  
+    return this.http.get<Product>(this.url + 'Api/Product/GetProductDetailsById/' + ID);  
+  }  
+  createOrder(csvrecord: CSVRecord): Observable<CSVRecord> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };  
+    return this.http.post<CSVRecord>(this.url+'api/ProductOrders',  
+    csvrecord, httpOptions);  
+  }  
+  getOrderDetails() : Observable<CSVRecord[]>
+  {    
+    return this.http.get<CSVRecord[]>(this.url+'api/ProductOrders');
+  }
+  getOrderDetailsbyId(id : Number): Observable<CSVRecord>
+  {
+    return this.http.get<CSVRecord>(this.url + ' api/ProductOrders/'+id);
+  }
+  
+} 
